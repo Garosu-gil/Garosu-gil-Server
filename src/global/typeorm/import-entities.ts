52def00f8ce412b0862d9f3eb19d4b0498fd9d15
over <80248568+over1234@users.noreply.github.com>
2022-10-05 21:09:08 +0900

    create(typeorm): entity module import

--- src/global/typeorm/import-entities.ts
+++ src/global/typeorm/import-entities.ts
@@ -1,1 +1,6 @@
-export const entities = [];
+import { Cafe } from '@core/cafe/cafe.entity';
+import { Category } from '@core/catagory/catagory.entity';
+import { Post } from '@core/post/post.entity';
+import { Tag } from '@core/tag/tag.entity';
+
+export const entities = [Post, Category, Tag, Cafe];
