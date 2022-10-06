6a24843e5e68fc47206819fdda17bf3930b87c1b
over <80248568+over1234@users.noreply.github.com>
2022-10-06 10:16:00 +0900

    delete[]

--- src/global/config/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as DataBaseConfiguration } from './database-configuration.loader';
-export * from './base-config.module';
-export * from './node-config.service';
--- src/global/typeorm/import-entities.ts
+++
@@ -1,6 +0,0 @@
-import { Cafe } from '@core/cafe/cafe.entity';
-import { Category } from '@core/catagory/catagory.entity';
-import { Post } from '@core/post/post.entity';
-import { Tag } from '@core/tag/tag.entity';
-
-export const entities = [Post, Category, Tag, Cafe];
