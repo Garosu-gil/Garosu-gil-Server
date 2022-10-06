d13b2f54e5c3226b0c08acae2d999e5a3fb41b26
over <80248568+over1234@users.noreply.github.com>
2022-10-06 10:17:11 +0900

    create(config): config 설정

---
+++ config/config.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv');
+const mysql = require('mysql');
+dotenv.config();
+const config = {
+  host: process.env.MYSQL_HOST,
+  port: '3306',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWARD,
+  database: 'garosu',
+  dialectOptions: {
+    useUTC: false,
+    dateStrings: true,
+    typecast: true,
+    timeZone: '+09:00',
+  },
+  timeZone: '+09:00',
+};
+
+const conn = mysql.createConnection(config);
+
+module.exports = { conn };
